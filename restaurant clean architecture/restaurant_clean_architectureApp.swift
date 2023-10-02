dfc696850866717ac9b6a4c005876501dd47e84f
fadhlialfarisi <fadhlialfarisi46@gmail.com>
2023-10-02 19:58:33 +0700

    Initial Commit

---
+++ restaurant clean architecture/restaurant_clean_architectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  restaurant_clean_architectureApp.swift
+//  restaurant clean architecture
+//
+//  Created by fadhlialfarisi on 02/10/23.
+//
+
+import SwiftUI
+
+@main
+struct restaurant_clean_architectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
